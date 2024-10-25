584c9415827ae7b3efaae668508840814fb3a5a1
MutluHamanakaLab <158519394+MutluHamanakaLab@users.noreply.github.com>
2024-10-24 20:44:47 -0500

    Update and rename Figure-3C.R to Figure-3C_3D.R

--- Figure-3C_3D.R
+++ Figure-3C_3D.R
@@ -21,7 +21,7 @@ gene_list_tgfb_vs_tgfb_torin<- na.omit(original_gene_list_tgfb_vs_tgfb_torin)
 gene_list_tgfb_vs_tgfb_torin<-sort(gene_list_tgfb_vs_tgfb_torin, decreasing = TRUE)
 gene_list_tgfb_vs_tgfb_torin_df<- as.data.frame(gene_list_tgfb_vs_tgfb_torin)
 
-# Save the ranked gene list for GSEA for Figure 3C and 3D
+# Save the ranked gene list for GSEA for Figure 3C and 3D (GSEA software preranked)
 write.table(gene_list_tgfb_vs_tgfb_torin_df, sep = "\t", quote = FALSE, row.names =TRUE, col.names = FALSE, "/PATH/TO/OUTPUT/GSEA_tgfb_vs_tgfb_torin.rnk")
 
 # Load organism-specific library
