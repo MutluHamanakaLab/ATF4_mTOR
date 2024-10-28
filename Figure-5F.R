470debd6b7e7ea214ba43b49ce19f6dd72f4adc0
MutluHamanakaLab <158519394+MutluHamanakaLab@users.noreply.github.com>
2024-10-28 16:29:22 -0500

    Update Figure-5F.R

--- Figure-5F.R
+++ Figure-5F.R
@@ -1,42 +1,59 @@
+## Load Necessary Libraries
+library(tidyverse)
+library(Seurat)
+library(decoupleR)
+library(ggplot2)
+
+## Read Seurat Object
+# Load the Seurat object
 level4.seurat_3 <- readRDS("/PATH/TO/FILE/level4.seurat_3.rds")
 
-net_tri <- get_collectri(organism='human', split_complexes=FALSE)
-# Extract the normalized log-transformed counts
+# Set the cluster identities to the desired order
+Idents(level4.seurat_3) <- factor(Idents(level4.seurat_3), levels = c("Fibrotic", "Inflammatory", "Alveolar"))
+
+## Network Analysis
+# Retrieve CollecTRI network data for human
+net_tri <- get_collectri(organism = 'human', split_complexes = FALSE)
+
+# Extract counts from the RNA assay
 mat <- as.matrix(level4.seurat_3@assays$RNA@data)
 
-# Run ulm
-acts <- run_ulm(mat=mat, net=net_tri, .source='source', .target='target',
-                .mor='mor', minsize = 5)
+# Run ULM
+acts <- run_ulm(mat = mat, net = net_tri, .source = 'source', .target = 'target', .mor = 'mor', minsize = 5)
 
+## Data Preparation
 data <- level4.seurat_3
 
-# Extract ulm and store it in tfsulm in pbmc
+# Extract ULM results and store them in a new assay called tfsulm
 data[['tfsulm']] <- acts %>%
-  pivot_wider(id_cols = 'source', names_from = 'condition',
-              values_from = 'score') %>%
+  pivot_wider(id_cols = 'source', names_from = 'condition', values_from = 'score') %>%
   column_to_rownames('source') %>%
   Seurat::CreateAssayObject(.)
 
-# Change assay
+# Change the default assay to tfsulm
 DefaultAssay(object = data) <- "tfsulm"
 
 # Scale the data
 data <- ScaleData(data)
+
+# Transfer the scaled data to the data slot
 data@assays$tfsulm@data <- data@assays$tfsulm@scale.data
 
-TF_Interest <- c("SMAD3", "SMAD4","ATF4", "HIF1A", "YY1")
+## Visualization of Transcription Factor Activities
+# Define transcription factors of interest
+TF_Interest <- c("SMAD3", "SMAD4", "ATF4", "HIF1A", "YY1")
 
-feature_plots <- (FeaturePlot(data, features = TF_Interest) & 
-  scale_colour_gradient2(low = 'blue', mid = 'white', high = 'red'))
-ggsave("/PATH/TO/OUTPUT/Figure-5FscFeaturePlot_level4_3_TF_act.png", feature_plots, width = 7, height = 10.5)
+# Create FeaturePlot for the transcription factors and save the plot
+feature_plots <- (FeaturePlot(data, features = TF_Interest) & scale_colour_gradient2(low = 'blue', mid = 'white', high = 'red'))
+ggsave("/PATH/TO/OUTPUT/Figure-5F_FeaturePlot.png", feature_plots, width = 7, height = 10.5)
 
+## Dot Plot for Transcription Factor Activities
+# Calculate plot width based on the number of transcription factors
 W <- length(TF_Interest) * 1.1
 
-
-scDotplot<- (DotPlot(data, features = TF_Interest) &
-  scale_colour_gradient2(low = 'blue', mid = 'white', high = 'red')) +
-  RotatedAxis()
-H <- length(unique(level4.seurat_3@active.ident)) *1.4
-ggsave("./output/2024-10-09/scDotplot_level4_3_TF_act_20241009.png",scDotplot, width = W, height = H)
-H <- length(unique(level4.seurat_3@active.ident)) *0.7
-ggsave("./output/2024-10-09/scDotplot_level4_3_TF_act_LOW_20241009.png",scDotplot, width = W, height = H)
+# Create DotPlot for the transcription factors and save high and low height versions
+scDotplot <- (DotPlot(data, features = TF_Interest) & scale_colour_gradient2(low = 'blue', mid = 'white', high = 'red')) + RotatedAxis()
+H <- length(unique(level4.seurat_3@active.ident)) * 1.4
+ggsave("/PATH/TO/OUTPUT/Figure-5F_Dotplot_HIGH.png", scDotplot, width = W, height = H)
+H <- length(unique(level4.seurat_3@active.ident)) * 0.7
+ggsave("/PATH/TO/OUTPUT/Figure-5F_Dotplot_LOW.png", scDotplot, width = W, height = H)
